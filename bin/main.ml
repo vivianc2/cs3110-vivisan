aea9400534057531d16e77af0907d3c191d145f7
Vivian <yc833@cornell.edu>
2022-10-26 00:08:41 -0400

    Make JSON file and process it
    - Added a file in data that has the information for our statements
    - Added functions in main.ml to process these data

--- bin/main.ml
+++ bin/main.ml
@@ -6,7 +6,7 @@ open Statement
 (* These should probably be put into a seperate file *)
 let count = ref 1
 
-let stm1 =
+(* let stm1 =
   make_stm
     (exp_of_string "3*y", exp_of_string "3*(x+3)")
     [ (exp_of_string "y", exp_of_string "x+3") ]
@@ -36,7 +36,19 @@ let stm4 =
     ]
     
 
-let stm_lst = [ stm1; stm2; stm3 ; stm4]
+let stm_lst = [ stm1; stm2; stm3 ; stm4] *)
+
+
+let data_dir_prefix = "data" ^ Filename.dir_sep
+let member = Yojson.Basic.Util.member
+let to_string = Yojson.Basic.Util.to_string
+
+let tuple_of_json j = ((j|>member "LHS"|>to_string|>exp_of_string),(j|>member "RHS"|>to_string|>exp_of_string))
+
+let stm_of_json j = make_stm (j|>member "equation"|>tuple_of_json) (j|>member "equals"|> Yojson.Basic.Util.to_list |>List.map tuple_of_json)
+
+let stm_lst_of_json j = j |> member "proofs" |> Yojson.Basic.Util.to_list |> List.map stm_of_json
+
 
 (** [play_game f] starts the adventure in file [f]. *)
 let rec play_game stm =
@@ -70,6 +82,7 @@ let rec play_game stm =
       exit 0
   | new_stm -> play_game new_stm
 
+
 let rec go_through_stm_lst stm_lst =
   match stm_lst with
   | [] ->
@@ -92,22 +105,41 @@ let rec go_through_stm_lst stm_lst =
           incr count;
           go_through_stm_lst t)
 
+
+let rec load_file f = 
+  match stm_lst_of_json (Yojson.Basic.from_file f) with 
+  | stm_lst -> (
+    ANSITerminal.print_string [ ANSITerminal.Bold ]
+      "Valid operations include thses techniques: ";
+    ANSITerminal.print_string
+      [ ANSITerminal.green; ANSITerminal.Bold ]
+      "refl, rw [variable_name].\n";
+    ANSITerminal.print_string [ ANSITerminal.Bold ]
+      "Other commands you can use include: ";
+    ANSITerminal.print_string
+      [ ANSITerminal.green; ANSITerminal.Bold ]
+      "help, quit, retry\n\n";
+    go_through_stm_lst stm_lst)
+  | exception e -> (
+      print_endline "This file doesn't exist. Please enter the correct name.";
+      print_string "> ";
+      match read_line () with
+      | exception End_of_file -> ()
+      | file_name -> load_file (data_dir_prefix ^ file_name ^ ".json"))
+        
+
 (** [main ()] prompts for the game to play, then starts it. *)
 let main () =
   ANSITerminal.print_string
     [ ANSITerminal.red; ANSITerminal.Bold ]
     "\n\nWelcome to Mathematical Prover!\n\n";
-  ANSITerminal.print_string [ ANSITerminal.Bold ]
-    "Valid operations include thses techniques: ";
-  ANSITerminal.print_string
-    [ ANSITerminal.green; ANSITerminal.Bold ]
-    "refl, rw [variable_name].\n";
-  ANSITerminal.print_string [ ANSITerminal.Bold ]
-    "Other commands you can use include: ";
-  ANSITerminal.print_string
-    [ ANSITerminal.green; ANSITerminal.Bold ]
-    "help, quit, retry\n\n";
-  go_through_stm_lst stm_lst
+  print_endline "Please enter the name of the game file you want to load.\n";
+  print_string "> ";
+  match read_line () with
+  | exception End_of_file -> ()
+  | file_name -> load_file (data_dir_prefix ^ file_name ^ ".json")
+ 
+
 
 (* Execute the game engine. *)
 let () = main ()
