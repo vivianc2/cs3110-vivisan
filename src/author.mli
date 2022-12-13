685712de2c09de7098432c4f27266c9ed6409eb8
Santigao Lai <zl345@cornell.edu>
2022-12-13 15:26:25 -0500

    Changed Author.mli

--- src/author.mli
+++ src/author.mli
@@ -27,6 +27,3 @@
    - Vivian Chen
 
  ************************************************************)
-
-val hours_worked : int
-(** [hours_worked] is the number of hours you worked on this assignment. *)
